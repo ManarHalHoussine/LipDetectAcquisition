f35bcf854f3e81226e513a547b703e94e81ce5dd
ManarHalHoussine <manarhalhoussine03@gmail.com>
2025-05-22 18:42:19 +0200

    second commit

--- lipreadingSite/settings.py
+++ lipreadingSite/settings.py
@@ -33,7 +33,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'lipreadingSite.urls'
@@ -81,8 +80,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'accounts', 'static')]
 
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Médias
